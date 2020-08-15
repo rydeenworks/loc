86d11eb7ade8e3a883d1528e913d167d6837c390
RydeenWorks <rydeenworks@gmail.com>
2020-08-16 08:21:11 +0900

    ファイル先頭の空行をカウントできる、解析結果詳細表示できる

--- loc.c
+++ loc.c
@@ -5,6 +5,149 @@
 #include <stdbool.h>
 #include <string.h>
 
+// コメント解析トークン型
+typedef enum {
+              TK_NEW_LINE,      // 改行
+              TK_LINE_COMMENT,  // １行コメント
+              TK_START_COMMENT, // 範囲コメント開始
+              TK_END_COMMENT,   // 範囲コメント終了
+              TK_OTHRE,         // 上記以外
+}TokenKind;
+
+typedef struct CommentToken CommentToken;
+
+struct CommentToken{
+  TokenKind kind;
+  CommentToken *next;
+  CommentToken *prev;
+};
+
+CommentToken *new_token(TokenKind kind, CommentToken* cur) {
+  CommentToken *tok = calloc(1, sizeof(CommentToken));
+  tok->kind = kind;
+  cur->next = tok;
+  tok->prev = cur;
+  return tok;
+}
+
+
+CommentToken *tokenize(char *buf, size_t size) {
+  CommentToken head;
+  head.kind = TK_NEW_LINE; //ファイル先頭は改行直接として処理する
+  head.next = NULL;
+  head.prev = NULL;
+  CommentToken *cur = &head;
+
+  char *c = buf;
+  for (size_t i = 0; i < size; i++, c++) {
+    switch(cur->kind) {
+    case TK_NEW_LINE:
+    case TK_END_COMMENT:
+    case TK_OTHRE:
+      // 空白を全て無視して処理しない('\n'除く)
+      if(isspace(*c)) {
+        // 改行の数でファイル全行数を計測する
+        if (*c == '\n') {
+          cur = new_token(TK_NEW_LINE, cur);
+        } else {
+          //nop
+        }
+      } else {
+        if (strncmp("//", c, 2) == 0) {
+          cur = new_token(TK_LINE_COMMENT, cur);
+          c++; i++;
+        } else if (strncmp("/*", c, 2) == 0) {
+          cur = new_token(TK_START_COMMENT, cur);
+          c++; i++;
+        } else if (strncmp("*/", c, 2) == 0) {
+          cur = new_token(TK_END_COMMENT, cur);
+          c++; i++;
+        } else {
+          if (cur->kind != TK_OTHRE)
+            cur = new_token(TK_OTHRE, cur);
+        }
+      }
+      break;
+    case TK_LINE_COMMENT:
+    case TK_START_COMMENT:
+      if (*c == '\n') {
+        cur = new_token(TK_NEW_LINE, cur);
+      } else if (strncmp("*/", c, 2) == 0 ) {
+        cur = new_token(TK_END_COMMENT, cur);
+        c++; i++;
+      }
+      break;
+    }
+  }
+
+  head.next->prev = NULL;
+  return head.next;
+}
+
+void count(CommentToken *tok, int *l, int *b, int *c) {
+  // 空白のみの行は空行とする
+  // 改行は'\n'
+  // 1行コメント "//" (行頭の場合のみカウント)
+  // 範囲コメント "/* ... */" (行頭の場合のみカウント)
+  int lines = 1;
+  int blank = 0;
+  int comment = 0;
+  printf("%d ", lines);
+  bool is_comment_scope = false;
+  for(CommentToken *cur=tok; cur->next != NULL; cur = cur->next ) {
+    switch(cur->kind) {
+    case TK_NEW_LINE:      // 改行直後
+      // ファイル先頭で改行した場合は空行とする
+      if (!cur->prev) {
+        blank++;
+        printf("b ");
+      }
+      printf("\n%d ", ++lines);
+      // 範囲コメント内の改行はコメント行として加える
+      if (is_comment_scope) {
+        comment++;
+        printf("c ");
+      } else {
+        // 改行後にすぐ改行した場合は空行とする
+        if (!cur->next || cur->next->kind == TK_NEW_LINE) {
+          blank++;
+          printf("b ");
+        }
+      }
+      break;
+    case TK_LINE_COMMENT:  // １行コメント以降
+      // 範囲コメント内の１行コメントは無視する
+      if (!is_comment_scope && (!cur->prev || cur->prev->kind == TK_NEW_LINE)) {
+        printf("c ");
+        printf("//\t");
+        comment++;
+      }
+      break;
+    case TK_START_COMMENT: // 範囲コメント開始
+      if (!is_comment_scope && (!cur->prev || cur->prev->kind == TK_NEW_LINE)) {
+        printf("c ");
+        printf("/*\t");
+        comment++;
+      }
+      is_comment_scope = true;
+      break;
+    case TK_END_COMMENT: // 範囲コメント終了
+      printf("*/\t");
+      is_comment_scope = false;
+      break;
+    case TK_OTHRE:         // 上記以外
+      printf("...\t");
+      break;
+    }
+  }
+  printf("\n");
+
+  *l = lines;
+  *b = blank;
+  *c = comment;
+}
+
+
 void error_at(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
@@ -13,14 +156,6 @@ void error_at(char *fmt, ...) {
   exit(1);
 }
 
-// 32bitで 空行 コメント行 コード行 を表現する
-// 表現可能な行数は以下の通り
-// blank  : 9bit(0-515)
-// comment: 9bit(0-515)
-// code   :14bit(0-16383)
-// 21098765432109876543210987654321
-// [  blank][comment][        code]
-
 // 8bitで 空行 コメント行 コード行 を表現する(シェルの$?で取得するため)
 // 表現可能な行数は以下の通り
 // blank  : 2bit(0-3)
@@ -28,7 +163,6 @@ void error_at(char *fmt, ...) {
 // Code   : 3bit(0-7)
 // 87654321
 // bbcccCCC
-
 
 int main(int argc, char* argv[])
 {
@@ -51,46 +185,10 @@ int main(int argc, char* argv[])
   int line = 0;
   int blank_line = 0;
   int comment_line = 0;
-  bool is_file_top = true; // ファイル先頭の場合true 改行または空白以外の出現以降はfalse
-  bool is_new_line = false; // 改行直後はtrue 改行後に空白以外の出現以降はfalse(連続した改行を判定するためのフラグ)
-  bool is_comment_scope = false; // 範囲コメントのスコープ内の場合はtrue
-  char *c = buf;
-  for (size_t i = 0; i < size; i++, c++) {
-    // 空白のみの行は空行とする
-    // 行頭の空白は常に無視する
-    // 改行は'\n'
-    // 1行コメント "//" (行頭が１行コメントの場合のみカウント)
-    // 範囲コメント "/* ... */" (1行目は行頭が範囲コメントの場合のみカウント)
-    // 範囲コメント内の１行コメントは無視する
-    // １行コメント後の範囲コメントは無視する
 
-    // 空白を全て無視して処理しない('\n'除く)
-    if(isspace(*c)) {
-      // ファイル全行数の計測
-      if (*c == '\n') {
-        line++;
-        // 範囲コメント内の改行はコメント行として加える
-        if (is_comment_scope)
-          comment_line++;
-        // 改行後にすぐ改行した場合は空行とする
-        if (is_new_line)
-          blank_line++;
-        is_new_line = true;
-      }
-    } else {
-      if (strncmp("//", c, 2) == 0) {
-        if( is_file_top || is_new_line)
-          comment_line++;
-      } else if (strncmp("/*", c, 2) == 0) {
-        if( is_file_top || is_new_line)
-          comment_line++;
-        is_comment_scope = true;
-      } else if (strncmp("*/", c, 2) == 0 ) {
-        is_comment_scope = false;
-      }
-      is_new_line = false;
-    }
-  }
+  CommentToken *tok = tokenize(buf, size);
+  count(tok, &line, &blank_line, &comment_line);
+
   free(buf);
 
   int code = line - blank_line - comment_line;
--- test.sh
+++ test.sh
@@ -22,6 +22,39 @@ code=0
 assert $val "./sample/line_comment_top.c"
 assert $val "./sample/scope_comment_top.c"
 
+blank=0
+comment=7
+code=0
+((val=$((blank<<5))+$((comment<<3))+$code))
+assert $val "./sample/scope_comment_7.c"
+
+blank=2
+comment=5
+code=0
+((val=$((blank<<5))+$((comment<<3))+$code))
+assert $val "./sample/scope_comment_5.c"
+
+
+blank=3
+comment=4
+code=0
+((val=$((blank<<5))+$((comment<<3))+$code))
+assert $val "./sample/line_comment_and_blank.c"
+
+blank=6
+comment=0
+code=1
+((val=$((blank<<5))+$((comment<<3))+$code))
+assert $val "./sample/blank6.c"
+
+blank=2
+comment=0
+code=0
+((val=$((blank<<5))+$((comment<<3))+$code))
+assert $val "./sample/blank1.c"
+
+
+
 #assert 48 "./sample/hello.c"
 #assert 6 "./sample/comment.c"
 
